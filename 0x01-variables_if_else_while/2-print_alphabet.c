37b08745f4aa89320d970f1f94d33954f355ee69
Abdulsalam1234-a <salamiabdullahi257@gmail.com>
2022-07-01 17:00:42 -0700

    pushing to git commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *  * main - Entry point
+ *   * Return: 0 - always
+ *    */
+int main(void)
+{
+	char alpha = 'a';
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar(10);
+	return (0);
+}
