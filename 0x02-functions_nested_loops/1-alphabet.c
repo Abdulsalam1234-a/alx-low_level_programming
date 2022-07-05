c5fa3ffd8efda737289ff2c8aacec4b1d99803e7
Abdulsalam1234-a <salamiabdullahi257@gmail.com>
2022-07-05 13:25:44 -0700

    1-alpabetc

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include "main.h" 
+
+/**
+ * main - check the code
+ *
+ * Return: nothing 
+*/
+int main(void)
+{
+	void print_alphabet(void);
+	return (0);
+}
+
+/**
+ * print_alphabet - prints a-z
+ *
+ * Return 0 success 
+*/
+void print_alphabet(void)
+{
+	int c;
+
+	for(c='a'; c<='z'; c++)
+		putchar(c);
+	return (0);
+}
+
