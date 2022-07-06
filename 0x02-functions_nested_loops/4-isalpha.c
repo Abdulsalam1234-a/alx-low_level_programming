72358a3b1ba0b0fad5c5f74f1b54e3616950b14d
Abdulsalam1234-a <salamiabdullahi257@gmail.com>
2022-07-06 00:03:13 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - check if it's alpha 
+ * @c: variable to print 
+ * Return: 0 if else
+*/
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
