2bf93b189df7901b56df8895b5b1366778cb3ea8
Abdulsalam1234-a <salamiabdullahi257@gmail.com>
2022-07-06 14:29:31 -0700

    add parameters

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h" 
+/**
+ * add - adding two numbers
+ * @a: first parameter
+ * @b: second parameter
+ * Return: c
+*/
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
+
