52f7c7a587a4d31eb4acb5c28302c51bcb4c34d4
Abdulsalam1234-a <salamiabdullahi257@gmail.com>
2022-07-06 00:21:37 -0700

    print_sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h" 
+/**
+ * print_sign - print the sign of numbers
+ * @n: the parameter to check
+ * Return: 0 at last
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n< 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
+
