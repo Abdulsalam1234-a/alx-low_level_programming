3acc66fae06eea00d65d0684bb8598adf2f41c8f
Abdulsalam1234-a <salamiabdullahi257@gmail.com>
2022-07-07 09:31:43 -0700

    main adjusted

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - tests function that print if interger is positive or negative
+ * main - tests function that prints if interger is positive or negative
  * Return: 0
 */
 
