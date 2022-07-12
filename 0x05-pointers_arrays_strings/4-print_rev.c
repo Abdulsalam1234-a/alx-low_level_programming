fdb262d8605d9c6f875edfb071a5a58b236c2463
Abdulsalam1234-a <salamiabdullahi257@gmail.com>
2022-07-12 12:08:09 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,6 +9,9 @@ void print_rev(char *s)
 {
 	int length;
 
+	for (length = 0; s[length] != '\0'; length++)
+
+
 	for (length = length - 1; length >= 0; length--)
 		_putchar(s[length]);
 	_putchar('\n');
