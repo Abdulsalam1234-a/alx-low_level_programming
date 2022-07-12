0029505896488991d97f7e6a9292d2164796ee78
Abdulsalam1234-a <salamiabdullahi257@gmail.com>
2022-07-12 10:37:23 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - printing a string followed by a newline to stdout
+ * @str: parameter to use
+*/
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(str);
+		str++;
+	}
+	_putchar("\n");
+}
