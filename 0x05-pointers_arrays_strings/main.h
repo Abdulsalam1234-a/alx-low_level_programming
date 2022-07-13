b6e241fe24441b78cf8048507e1829ae7da019fe
Abdulsalam1234-a <salamiabdullahi257@gmail.com>
2022-07-12 21:55:39 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -11,6 +11,9 @@ void print_rev(char *s);
 void puts2(char *str);
 void rev_string(char *s);
 void print_array(int *a, int n);
-char *_strcpy(char *dest, char *sec);
+char *_strcpy(char *dest, char *sec)
+;
+int _atoi(char *s);
+
 
 #endif /*MAIN_H*/
