c8b78182641f879c3f6f44eda7308436263317c9
Abdulsalam1234-a <salamiabdullahi257@gmail.com>
2022-07-14 14:36:21 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,11 +1,16 @@
 #include "main.h"
+/**
+ * *leet - to check
+ * @s: the parameter to use
+ * Return: s
+ */
 char *leet(char *s)
 {
 	int a = 0, b = 0, l = 5;
 	char r[5] = {'A', 'E', 'O', 'T', 'L'};
 	char n[5] = {'4', '3', '0', '7', '1'};
 
-	while (s[9])
+	while (s[a])
 	{
 		b = 0;
 
