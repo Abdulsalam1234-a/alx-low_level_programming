2f9e38b2715ece674479356ceb87acd4573c2b1a
Abdulsalam1234-a <salamiabdullahi257@gmail.com>
2022-07-14 15:36:40 -0700

    1⁰0-__

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *rot13 - a function to decode
+ * @s: parame to check
+ * Return: parameter
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzancdefghtjklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i= 0; *(s + i); i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (a[j] == *(s + i))
+			{
+				*(s + i) = b[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
+
