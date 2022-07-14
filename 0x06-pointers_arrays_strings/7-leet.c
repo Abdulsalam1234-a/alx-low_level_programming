cdd83ac40998abe72114827ebdc672fdbe895977
Abdulsalam1234-a <salamiabdullahi257@gmail.com>
2022-07-14 14:30:52 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+char *leet(char *s)
+{
+	int a = 0, b = 0, l = 5;
+	char r[5] = {'A', 'E', 'O', 'T', 'L'};
+	char n[5] = {'4', '3', '0', '7', '1'};
+
+	while (s[9])
+	{
+		b = 0;
+
+		while (b < l)
+		{
+			if (s[a] == r[b] || s[a] - 32 == r[b])
+			{
+				s[a] = n[b];
+			}
+			b++;
+		}
+		a++;
+	}
+	return (s);
+}
