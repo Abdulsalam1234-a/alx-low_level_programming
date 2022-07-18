3018579c916f197fbb2d6cb4d2e721734353df7d
Abdulsalam1234-a <salamiabdullahi257@gmail.com>
2022-07-18 13:23:53 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strspn - function
+ * @s: character to pointer s
+ * @accept: the accepting value
+ * Return: z
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int z = 0, x, y;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		if (s[x] != 32)
+		{
+			for (y = 0; accept[y] != '\0'; y++)
+			{
+				if (s[x] == accept[y])
+					z++;
+			}
+		}
+		else
+			return (z);
+	}
+	return (z);
+}
