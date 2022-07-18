5b94f476d614e2255b2852fc234ace7c5d9961c1
Abdulsalam1234-a <salamiabdullahi257@gmail.com>
2022-07-18 13:40:50 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,7 +4,9 @@
  * _strchr - locate a character in a string
  * @s: string to search
  * @c: character to find
- * Return: a pointer to the first occurrence of the character c in the string s, or NULL if the character is not found
+ * Return: a pointer to the first occurrence
+ * of the character c in the string s,
+ *or NULL if the character is not found
  */
 char *_strchr(char *s, char c)
 {
---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h" 
+#include <stdio.h>
+/**
+ * _strpbrk - function break
+ * @s: character s
+ * @accept: accepting string
+ * Return: NULL if not
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i	= 0; *s != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (*s == accept[j])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return (NULL);
+}
